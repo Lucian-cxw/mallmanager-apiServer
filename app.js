80e4bf6c4d28a147f2360244d140961e55bc67a8
Lucian-cxw <616730488@qq.com>
2021-05-27 17:08:05 +0800

    处理分页请求接口

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express=require("express")
+const handleDB=require("./db/handleDB")
 const cors= require("cors")
 
 
@@ -10,7 +11,7 @@ app.use(express.json());
 app.post("/login",(req,res)=>{
     // {data,meta:{msg,status}}=res.data  
     let {password,username}=req.body
-    console.log(req.body)
+    // console.log(req.body)
   
     if(!username||!password){
         let result={
@@ -57,6 +58,33 @@ app.post("/login",(req,res)=>{
     
 })
 
+app.get("/users",(req,res)=>{
+    (async function(){
+        // 接受参数
+        // pageNum 当前页码 不能为空
+        // pageSize 每页显示条数 不能为空
+       let {pageNum,pageSize}=req.query
+       console.log(req.query)
+
+       console.log(pageNum,pageSize)
+
+       let users= await handleDB(res,"user","sql","数据库查询出错","select username,password_hash,mobile,email,create_time from user") 
+       let total= await handleDB(res,"user","sql","数据库查询出错","select count(*) from user") 
+    //    返回参数
+        let result={
+            data:{
+                users,
+                total:total[0]['count(*)']
+            },
+            meta:{
+                status:200,
+                msg:"获取数据库成功"
+            }
+        }
+       console.log(result)
+       res.send(result)
+    })()
+})
 app.listen(3000,()=>{
     console.log("正在监听3000端口")
 })
